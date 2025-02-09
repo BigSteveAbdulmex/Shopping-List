57a459b164688c52d24f2c8b08eee7314a61e94d
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-09 22:43:40 +0100

    Added some missing imports

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_app/data/dummy_items.dart';
 
 import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
-  const GroceryList({
-    super.key,
-  });
+  const GroceryList({super.key});
 
   @override
   State<GroceryList> createState() => _GroceryListState();
@@ -40,22 +37,18 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
-    Widget content = const Center(
-      child: Text('No items yet. Add some!'),
-    );
+    Widget content = const Center(child: Text('No items added yet.'));
 
-    if (groceryItems.isNotEmpty) {
+    if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
         itemCount: _groceryItems.length,
         itemBuilder: (ctx, index) => Dismissible(
-          key: ValueKey(_groceryItems[index].id),
           onDismissed: (direction) {
             _removeItem(_groceryItems[index]);
           },
+          key: ValueKey(_groceryItems[index].id),
           child: ListTile(
-            title: Text(
-              _groceryItems[index].name,
-            ),
+            title: Text(_groceryItems[index].name),
             leading: Container(
               width: 24,
               height: 24,
@@ -71,7 +64,7 @@ class _GroceryListState extends State<GroceryList> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Groceries'),
+        title: const Text('Your Groceries'),
         actions: [
           IconButton(
             onPressed: _addItem,
