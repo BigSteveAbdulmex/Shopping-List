fa5bf748691497c513c9cf788c67782286194ada
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-11 19:37:16 +0100

    Added logic to test and see the response from the backend

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -23,13 +23,13 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https(
           'shopping-list-app-25656-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      http.post(
+      final response = await http.post(
         url,
         headers: {
           'Content-Type': 'application/json',
@@ -42,6 +42,14 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
+
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
     }
   }
 
